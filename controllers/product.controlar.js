68cf0bba1a461e77a8fecc2cea781c9c6ef8b267
Md- Hasmat <mdhasmat690>
2022-09-18 13:15:30 +0600

    module 7 porjoto

--- controllers/product.controlar.js
+++ controllers/product.controlar.js
@@ -1,6 +1,13 @@
+const { isValidObjectId } = require("mongoose");
+const Product = require("../models/Product");
 const {
   getProductsService,
   createProductService,
+  updateProductService,
+  bulkUpdateProductService,
+  updateProductByIdService,
+  deleteProductById,
+  bulkDeleteProductService,
 } = require("../services/product.service");
 
 exports.getProducts = async (req, res, next) => {
@@ -38,6 +45,83 @@ module.exports.createProduct = async (req, res, next) => {
     res.status(400).json({
       status: "fail",
       message: "Data is not inserted",
+      error: error.message,
+    });
+  }
+};
+
+exports.updateProductById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await updateProductByIdService(id, req.body);
+    res.status(200).json({
+      status: "success",
+      message: "successfully data updated",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "Could't not updated data",
+      error: error.message,
+    });
+  }
+};
+
+exports.bulkUpdateProduct = async (req, res, next) => {
+  try {
+    const result = await bulkUpdateProductService(req.body);
+    res.status(200).json({
+      status: "success",
+      message: "successfully data updated",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "Could't not updated data",
+      error: error.message,
+    });
+  }
+};
+
+exports.deleteProductById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    /* if (isValidObjectId(id)) {
+      console.log("lkad;alksjdffaskjfd");
+    } */
+    const result = await deleteProductById(id);
+
+    if (!result.deletedCount) {
+      return res.status(400).json({
+        status: "fail",
+        error: "could not delete the product",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "successfully data Deleted",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "Could't not delete data",
+      error: error.message,
+    });
+  }
+};
+
+exports.bulkDeleteProduct = async (req, res, next) => {
+  try {
+    const result = await bulkDeleteProductService(req.body.ids);
+    res.status(200).json({
+      status: "success",
+      message: "successfully deleted the given product",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: "Could't not updated data",
       error: error.message,
     });
   }
--- services/product.service.js
+++ services/product.service.js
@@ -9,3 +9,44 @@ exports.createProductService = async (data) => {
   const product = await Product.create(data);
   return product;
 };
+exports.updateProductByIdService = async (productId, data) => {
+  /* const result = await Product.updateOne(
+    { _id: productId },
+    { $set: data },
+    { runValidators: true }
+  ); */
+
+  const result = await Product.updateOne(
+    { _id: productId },
+    { $inc: data },
+    { runValidators: true }
+  );
+
+  /* const product = await Product.findById(productId);
+  const result = product.set(data).save(); */
+  return result;
+};
+
+exports.bulkUpdateProductService = async (data) => {
+  /*  const result = await Product.updateMany({ _id: data.ids }, data.data, {
+    runValidators: true,
+  }); */
+
+  const products = [];
+  data.ids.forEach((product) => {
+    products.push(Product.updateOne({ _id: product.id }, product.data));
+  });
+  const result = await Promise.all(products);
+  console.log(result);
+  return result;
+};
+
+exports.deleteProductById = async (id) => {
+  const result = await Product.deleteOne({ _id: id });
+  return result;
+};
+
+exports.bulkDeleteProductService = async (ids) => {
+  const result = await Product.deleteMany({ _id: ids });
+  return result;
+};
